7f22773f43a01ffe11f33abc14da81015ff5dc33
hafijure <hafijure@gmail.com>
2022-08-03 13:13:00 +0600

    number guessing game v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from 'react';
 
 function App() {
+  let random = Math.floor(Math.random() * 10) + 1;
+  const [userGuess, setUserGuess] = useState('');
+  const [userCount, setUserCount] = useState(0);
+  const [userAllGuessValue, setUserAllGuessValue] = useState([])
+  const [randomNumber, setRandomNumber] = useState(random)
+  const [msg, setMsg] = useState('')
+  const [disabled, setDisabled] = useState(false)
+  
+
+  const handleValueChange = (e) => {
+    setUserGuess(e.target.value)
+  }
+
+  const submitHandler = () => {
+    if (Number(randomNumber) === Number(userGuess)) {
+      setMsg('Congratulations !!!')
+      setDisabled(true)
+    }else if(userCount === 4){
+      setMsg('Game is Over !1!')
+      setDisabled(true)
+    }else if(Number(randomNumber) < Number(userGuess)){
+      setMsg('Worng guess! It may be little lower')
+    }else if(Number(randomNumber) > Number(userGuess)){
+      setMsg('Worng guess! It may be little higher')
+    }else if(Number(userGuess) > 10){
+      setMsg('Worng Guess !!!')
+    }
+    setUserCount(userCount + 1)
+    setUserAllGuessValue([...userAllGuessValue,userGuess])
+  }
+
+  const startAgain = () => {
+    setDisabled(false)
+    setUserCount(0)
+    setUserGuess('')
+    setMsg('')
+    setUserAllGuessValue([])
+    setRandomNumber(Math.floor(Math.random() * 10) + 1)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h1>Number Gussing Game</h1>
+      <input disabled={disabled} value={userGuess} type="text" onChange={handleValueChange} />
+      <br/>
+      <button disabled={disabled} onClick={submitHandler}>Submit</button>
+      {disabled && 
+       <button onClick={startAgain}>Start Again</button>
+      }
+      <p>{msg}</p>
+      <p>Total Count: {userCount}</p>
+      <p>Random number: {randomNumber}</p>
+      <p>User Guess: {userAllGuessValue?.map((item,index) => {
+        return <span key={index}>{item}, { }</span>;
+      })}</p>
     </div>
   );
 }
